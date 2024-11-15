1d81998dd0c37e222b53088bc48ababa852db3fe
Timo Schneider <timoschneider04@gmail.com>
2024-11-15 22:13:38 +0100

    started strat

--- src/logic/strategy.rs
+++ src/logic/strategy.rs
@@ -1,13 +1,32 @@
+use std::sync::Arc;
 use crate::external_models::{game_state::GameState as ExternalGameState, player_action::PlayerAction};
+use crate::internal_models::base::Base;
 use crate::internal_models::game_state::GameState;
 
 pub fn decide(game_state: ExternalGameState) -> Vec<PlayerAction> {
 
     let game_state: GameState = GameState::from_external(game_state);
 
+    // sort bases
+    let mut own_bases: Vec<&Arc<Base>> = Vec::new();
+    let mut enemy_bases: Vec<&Arc<Base>> = Vec::new();
+
     game_state.bases.values().for_each(|base| {
+        if base.player == game_state.game.player {
+            own_bases.push(base)
+        }
+        else {
+            enemy_bases.push(base)
+        }
+    });
+
+    own_bases.iter().for_each(|own_base| {
+       enemy_bases.iter().for_each(|enemy_base| {
 
 
+
+
+       })
     });
 
     vec![PlayerAction {

5bd348526847e591f4e2aaa8049f7452cf35069d
Timo Schneider <timoschneider04@gmail.com>
2024-11-15 22:30:47 +0100

    added config

---
+++ src/logic/config.rs
@@ -0,0 +1,124 @@
+
+const MIN_REQUIREMENTS: [Requirement; 14] = [
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+    // Level: n
+    Requirement{
+        MIN_UNITS_AFTER_ATTACK: 0,
+        MIN_UNITS_FOR_ATTACK: 0,
+        MIN_UNITS_FOR_UPGRADE: 0,
+        MIN_UNITS_FOR_DISTRIBUTION: 0,
+        MAX_DISTANCE_FOR_ATTACK: 100,
+    },
+];
+
+struct Requirement {
+    MIN_UNITS_AFTER_ATTACK: u32,
+    MIN_UNITS_FOR_ATTACK: u32,
+    MIN_UNITS_FOR_UPGRADE: u32,
+    MIN_UNITS_FOR_DISTRIBUTION: u32,
+    MAX_DISTANCE_FOR_ATTACK: u32,
+}
+

6c18bd7ebc9cbb55f3fdd2e1d249377cc7533279
Timo Schneider <timoschneider04@gmail.com>
2024-11-16 00:25:17 +0100

    reduced memory usage

--- src/internal_models/game_state.rs
+++ src/internal_models/game_state.rs
@@ -23,13 +23,13 @@ impl GameState {
         };
 
         // create Hashmap for bases
-        let mut bases: HashMap<u32, Arc<Base>> = HashMap::with_capacity(10_000);
+        let mut bases: HashMap<u32, Arc<Base>> = HashMap::with_capacity(2_500);
         game_state.bases.iter().for_each(|base| {
             bases.insert(base.uid, Base::from_external(base, &game_config.base_levels));
         });
 
         // create Hashmap for attacks
-        let mut attacks: HashMap<Uuid, Arc<BoardAction>> = HashMap::with_capacity(50_000);
+        let mut attacks: HashMap<Uuid, Arc<BoardAction>> = HashMap::with_capacity(25_000);
         game_state.actions.iter().for_each(|attack| {
             attacks.insert(attack.uuid, BoardAction::from_external(attack, &bases, &game_config.paths));
         });

bbb4d4924fcef27303194de74ca486afb570af9f
Timo Schneider <timoschneider04@gmail.com>
2024-11-16 04:33:15 +0100

    ...

--- src/logic/config.rs
+++ src/logic/config.rs
@@ -18,40 +18,40 @@ pub const MIN_REQUIREMENTS: [Requirement; 14] = [
     },
     // Level: 80
     Requirement{
-        MIN_UNITS_AFTER_ATTACK: 12,
-        MIN_UNITS_FOR_ATTACK: 20,
+        MIN_UNITS_AFTER_ATTACK: 15,
+        MIN_UNITS_FOR_ATTACK: 25,
         MIN_UNITS_FOR_UPGRADE: 40,
         MIN_UNITS_FOR_DISTRIBUTION: 32,
         MAX_UNITS_FOR_SUPPLY: 20,
     },
     // Level: 100
     Requirement{
-        MIN_UNITS_AFTER_ATTACK: 16,
-        MIN_UNITS_FOR_ATTACK: 25,
+        MIN_UNITS_AFTER_ATTACK: 25,
+        MIN_UNITS_FOR_ATTACK: 40,
         MIN_UNITS_FOR_UPGRADE: 50,
         MIN_UNITS_FOR_DISTRIBUTION: 40,
         MAX_UNITS_FOR_SUPPLY: 24,
     },
     // Level: 200
     Requirement{
-        MIN_UNITS_AFTER_ATTACK: 32,
-        MIN_UNITS_FOR_ATTACK: 50,
+        MIN_UNITS_AFTER_ATTACK: 35,
+        MIN_UNITS_FOR_ATTACK: 80,
         MIN_UNITS_FOR_UPGRADE: 100,
         MIN_UNITS_FOR_DISTRIBUTION: 80,
         MAX_UNITS_FOR_SUPPLY: 50,
     },
     // Level: 300
     Requirement{
-        MIN_UNITS_AFTER_ATTACK: 45,
-        MIN_UNITS_FOR_ATTACK: 80,
+        MIN_UNITS_AFTER_ATTACK: 60,
+        MIN_UNITS_FOR_ATTACK: 100,
         MIN_UNITS_FOR_UPGRADE: 150,
         MIN_UNITS_FOR_DISTRIBUTION: 120,
         MAX_UNITS_FOR_SUPPLY: 60,
     },
     // Level: 400
     Requirement{
-        MIN_UNITS_AFTER_ATTACK: 75,
-        MIN_UNITS_FOR_ATTACK: 100,
+        MIN_UNITS_AFTER_ATTACK: 150,
+        MIN_UNITS_FOR_ATTACK: 200,
         MIN_UNITS_FOR_UPGRADE: 200,
         MIN_UNITS_FOR_DISTRIBUTION: 160,
         MAX_UNITS_FOR_SUPPLY: 80,
